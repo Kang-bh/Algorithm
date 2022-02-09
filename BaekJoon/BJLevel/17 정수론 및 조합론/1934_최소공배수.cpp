6de26626b687714c5839c4a75b9407a3dbf9cd36
BH-Kang <kabyho3513@gmail.com>
2022-02-10 04:48:32 +0900

    Add 1934: Least Common Multiple

---
+++ BaekJoon/BJLevel/17 정수론 및 조합론/1934_최소공배수.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int T; // testcase
+	cin >> T;
+	int num1, num2;
+
+	for (int i = 0; i << T; i++) {
+		cin >> num1 >> num2;
+		
+		int low_num, least, greatest;
+		low_num = (num1 < num2) ? num1 : num2; // »ïÇ× ¿¬»êÀÚ
+
+		for (int num = low_num; num > 0; num--) {
+			if (num1 % num == 0 && num2 % num == 0) {
+				least = num;
+				break;
+			}
+		}
+
+		cout << least * (num1 / least) * (num2 / least) << endl;
+	}
+
+
+	return 0;
+}
