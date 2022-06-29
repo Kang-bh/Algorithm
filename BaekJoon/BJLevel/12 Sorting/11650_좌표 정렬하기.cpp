9a21e552635a18bf27e2c4c7f079bd345db6639a
BH-Kang <kabyho3513@gmail.com>
2022-06-29 18:18:40 +0900

    Add 11650: 좌표 정렬하기

---
+++ BaekJoon/BJLevel/12 Sorting/11650_좌표 정렬하기.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<pair<int, int>> point;
+
+void Input(int N) {
+	int p1, p2;
+	for (int i = 0; i < N; i++) {
+		cin >> p1 >> p2;
+		point.push_back(make_pair(p1, p2));
+	}
+}
+
+void Print(int N) {
+	for (auto iter = point.begin(); iter != point.end(); iter++) {
+		cout << iter->first << " " << iter->second << "\n";
+	}
+}
+
+int main() {
+
+	int N;
+	cin >> N;
+	
+	Input(N);
+	sort(point.begin(), point.end());
+	Print(N);
+
+	return 0;
+}
