27fc7f8f96365f0566a1fd9976bf9f08af88c5e2
Kang-bh <kabyho3513@gmail.com>
2024-02-12 20:28:08 +0900

    Add 27323 : 직사각형

---
+++ BaekJoon/BJLevel/기하 1/27323_직사각형.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(int A, int B) {
+    cout << A * B << endl;
+} 
+
+int main() {
+    int A;
+    int B;
+
+    cin >> A;
+    cin >> B;
+
+    solve(A, B);
+}
