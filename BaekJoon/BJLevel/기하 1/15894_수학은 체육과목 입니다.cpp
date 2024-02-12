e0b3bd9dc042b5ea81f248f4a0d249fc220ee8a0
Kang-bh <kabyho3513@gmail.com>
2024-02-12 20:25:15 +0900

    Add 15894 : 수학은 체육과목 입니다.

---
+++ BaekJoon/BJLevel/기하 1/15894_수학은 체육과목 입니다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(long int N) {
+    cout << 4 * N << endl;
+}
+
+int main() {
+
+    long int N;
+    cin >> N;
+
+    solve(N);
+
+    return 0;
+}
