2019b0f889f0af2639b6c03f64392ed48f050d77
Kang-bh <kabyho3513@gmail.com>
2024-02-28 00:11:11 +0900

    Add 13909 : 창문 닫기

---
+++ BaekJoon/BJLevel/정수론 및 조합론/13909_창문 닫기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+void solve(long long int N) {
+    long long result = (long long int) sqrt(N);
+
+    cout << result << endl;
+}
+
+int main() {
+
+    long long int N;
+
+    cin >> N;
+
+    solve(N);
+    return 0;
+}
