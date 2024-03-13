ad171c71e567806528c1352369a7d36e1bb968e1
Kang-bh <kabyho3513@gmail.com>
2024-03-13 15:46:53 +0900

    Update 3-1 거스름돈 cpp

--- Coding_Test_with_Python/3. 그리디/3-1 거스름돈.cpp
+++ Coding_Test_with_Python/3. 그리디/3-1 거스름돈.cpp
@@ -16,12 +16,27 @@ void solve(int& N) {
     cout << result << endl;
 }
 
+int cnt = 0;
+int coin_types[4] = { 500, 100, 50, 10 }; // 화폐 단위를 반복수행
+
+void solve2(int& N) {
+    // solve1 에서의 코드 반복 줄일 수 있음.
+    for (int i = 0; i < 4; i++) {
+        cnt += N / coin_types[i];
+        N = N % coin_types[i];
+    }
+
+    cout << cnt << endl;
+
+}
+
 int main() {
     int N;
 
     cin >> N;
 
-    solve(N);
+    // solve(N);
+    solve2(N);
 
     return 0;
 }
