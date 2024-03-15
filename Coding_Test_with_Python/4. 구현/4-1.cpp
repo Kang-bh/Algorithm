9028ed1ab63ebba140ee27e69edf0bdd4a1aa681
Kang-bh <kabyho3513@gmail.com>
2024-03-15 20:26:51 +0900

    Update 4-1 상하좌우 cpp

--- Coding_Test_with_Python/4. 구현/4-1.cpp
+++ Coding_Test_with_Python/4. 구현/4-1.cpp
@@ -35,6 +35,46 @@ void move(char direction) {
     }
 }
 
+#include <iostream>
+#include <utility>
+#include <map>
+#include <string>
+#include <algorithm>
+using namespace std;
+// ~17
+
+map <char, int> table = { {'L', 0}, {'R', 1}, {'U', 2}, {'D', 3} };
+
+int location_x = 1;
+int location_y = 1;
+
+int direction_y[4] = { -1, 1, 0, 0 };
+int direction_x[4] = { 0, 0, -1, 1 };
+
+void solve(int N, string str) {
+
+    int idx, next_location_x, next_location_y;
+
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    for (int i = 0; i < str.length(); i++) {
+    
+        idx = table[str[i]];
+
+        next_location_x = location_x + direction_x[idx];
+        next_location_y = location_y + direction_y[idx];
+
+        if (next_location_x < N + 1 && next_location_x > 0)
+            location_x = next_location_x;
+        if (next_location_y < N + 1 && next_location_y > 0)
+            location_y = next_location_y;
+
+    }
+
+    cout << location_x << " " << location_y << endl;
+
+}
+
+
 int main () {
 
     cin >> N;
@@ -47,6 +87,8 @@ int main () {
     }
 
     cout << y << " " << x << endl;
+
+    solve(N, movePlan);
 
     return 0;
 }
